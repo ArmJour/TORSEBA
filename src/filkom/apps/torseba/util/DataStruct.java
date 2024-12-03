5b85e31c6cf72692ce469ef0dd962e71ddce1433
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-12-03 13:33:59 +0700

    Add valid user type and remove unused vars

--- src/filkom/apps/torseba/util/DataStruct.java
+++ src/filkom/apps/torseba/util/DataStruct.java
@@ -14,13 +14,17 @@ import java.util.*;
 public class DataStruct
 {
     public final int BOX_WIDTH = 200; /* Width of the box */
-    public final int MAX_DATA_AMOUNT = 100; /* Max data amount */
     public final String DEFAULT_HEADER = "Selamat Datang"; /* Default header */
     public final String DEFAULT_SUB_HEADER = "TORSEBA"; /* Default sub header */
-    public final String DEFAULT_CURRENCY = "Rp"; /* Default currency */
     public HashMap<String,String> userLoginData = new HashMap<>(); /* Email and password  */
     public HashMap<List<String>,List<String>> userData = new HashMap<>(); /* All user data */
-    public String[] tableHeaderData = /* Table header for displaying user / driver data */
+    public final String[] VALID_USER_OPTION =
+    {
+        /*"admin",*/ /* Disabled */
+        "tutor",
+        "tutee"
+    };
+    public final String[] TABLE_HEADER_DATA = /* Table header for displaying user data */
     {
         "Email",
         "Password",
